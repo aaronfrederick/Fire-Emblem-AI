83d15b56874987283338c9441a4c12dffbc1235d
Aaron Frederick <34990020+aaronfrederick@users.noreply.github.com>
2019-01-21 12:10:45 -0800

    separate chapter-specific functions

--- feaifuncs.py
+++ feaifuncs.py
@@ -37,7 +37,7 @@ def choose_option(options, random_opt=False):
         elif 'tem' in choice:
             press_key("s",ind)
             press_key("'")
-            use_item(random_item=True)
+            use_item()
             return 'Item'
         else:
             wait()
@@ -53,81 +53,26 @@ def choose_option(options, random_opt=False):
             if 'tem' in options[choice]:
                     press_key('s',choice)
                     press_key("'")
-                    use_item(random_item=True)
+                    use_item()
                     return 'Item'
             else:
                 wait()
                 return 'Wait'
 
+            
+def double_check_name():
+    screen = np.array(ImageGrab.grab(bbox=(125,297,175,332)))
+    for i, row in enumerate(screen):
+        for j, pixel in enumerate(row):
+            brightness = sum(pixel)
+            if brightness > 890:
+                screen[i][j] = np.array([0, 0, 0, 255], dtype=np.uint8)
+    return pytesseract.image_to_string(screen)
+            
 
 def enemy_phase_break(stall = 5):
     time.sleep(stall)
 
-    
-def gen_data_prologue():
-    taking_turns = True
-    while taking_turns:
-        state = []
-        act = []
-
-        #Grab Board State
-        select_next_unit()
-        press_key(';')
-        name = grab_name()
-        if not name:
-            break
-        act.append(name)
-        press_key('w')
-        press_key("'")
-        press_key('s')
-        press_key("'")
-        board_state = grab_board_state()
-        press_key(';',2)
-
-        metric = board_state[1]/board_state[2]
-        
-
-        state.append(board_state[0])
-        state.append(board_state[1])
-        state.append(board_state[2])
-
-        #Set Options on 1st Turn Only
-        if state[2] == 1:
-            set_options()
-            time.sleep(0.2)
-
-        #Execute Actions
-        select_next_unit()
-        press_key('e')
-        d = grab_stats()
-        df = pd.DataFrame(d, index=[name], columns=d.keys())
-        state.extend(df.values[0])
-        press_key(';')
-        moves = move_unit(random_move=True)
-        act.extend(list(moves))
-        opts = grab_options()
-        if not opts:
-            act.append("Invalid move")
-            break
-        else:
-            selection = choose_option(opts, random_opt=True)
-            act.append(selection)
-        
-        metrics.append(metric)
-        states.append(state)
-        actions.append(act)
-
-        if state[2] == 1:
-            time.sleep(5)
-            press_key('Enter')
-            time.sleep(1)
-        else:
-            enemy_phase_break(7)
-
-        #Break loop if turn count greater than 8
-        if state[2] > 8:
-            break    
-    
     
 def grab_board_state(model = 'block_text_logreg.pkl'):
     """
@@ -353,23 +298,6 @@ def processed_img(original_image):
     return processed_img
 
 
-def reset_to_prologue():
-    """
-    Assumes there is the resume chapter option
-    DO NOT USE FUNCTION WITHOUT THIS OPTION, IT WILL SELECT ERASE DATA
-    """ 
-    pyautogui.hotkey('command', 'r')
-    time.sleep(2.2)
-    press_key('enter', 2, 2)
-    press_key('s')
-    press_key("'", 2, 0.5)
-    time.sleep(1.8)
-    press_key('a')
-    press_key("'")
-    time.sleep(2)
-    press_key('enter', 4, 1.5)
-
-
 def select_next_unit():
     press_key('q')
     press_key("'")
@@ -400,7 +328,7 @@ def subscreen(x0,y0,x1,y1, screen):
     return sub_img
 
 
-def use_item(slot=0,random_item=False):
+def use_item(slot=1,random_item=False):
     if random_item:
         press_key('s',random.randint(0,5))
     else:
